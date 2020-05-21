0d6afe12daef9495d725aaa8bcc665333ce25363
dale <zhushihuacool@163.com>
2020-05-21 10:09:47 +0800

    Example of OrangePi sensor using WiringPi-Python library

---
+++ examples/orangepi-sensors/light.py
@@ -0,0 +1,22 @@
+import sys
+import time
+import wiringpi
+from wiringpi import GPIO
+
+PIN = 6 
+
+wiringpi.wiringPiSetup()
+wiringpi.pinMode(PIN, GPIO.OUTPUT)
+
+while True:
+	try:
+		wiringpi.digitalWrite(PIN, GPIO.HIGH)
+		print(wiringpi.digitalRead(PIN))
+		time.sleep(1)
+		wiringpi.digitalWrite(PIN, GPIO.LOW)
+		print(wiringpi.digitalRead(PIN))
+		time.sleep(1)
+	except KeyboardInterrupt:
+		print("\nexit")
+		sys.exit(0)
+
